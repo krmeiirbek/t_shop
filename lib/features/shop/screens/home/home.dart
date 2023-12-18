5c5cb0b5f204d96e1c5bdfeaeaec241fa4ccef71
kazybekmeiirbek <newkazybek@gmail.com>
2023-12-18 11:14:57 +0600

    Search Bar and Horizontal ListView builder Categories

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:t_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 
 import 'widgets/home_appbar.dart';
+import 'widgets/home_categories.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,7 +20,11 @@ class HomeScreen extends StatelessWidget {
               child: TPrimaryHeaderContainer(
                 child: Column(
                   children: [
-                    THomeAppbar(),
+                    const THomeAppbar(),
+                    SizedBox(height: TSizes.spaceBtwSections),
+                    const TSearchContainer(text: 'Дүкеннен іздеу'),
+                    SizedBox(height: TSizes.spaceBtwSections),
+                    const THomeCategories(),
                   ],
                 ),
               ),
---
+++ lib/features/shop/screens/home/widgets/home_categories.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/image_text_widgets/vertical_image_text.dart';
+import 'package:t_store/common/widgets/texts/section_heading.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+
+class THomeCategories extends StatelessWidget {
+  const THomeCategories({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: TSizes.defaultSpace),
+      child: Column(
+        children: [
+          const TSectionHeading(
+            title: 'Танымал категориялар',
+            showActionButton: false,
+            textColor: TColors.white,
+          ),
+          SizedBox(height: TSizes.spaceBtwItems),
+          SizedBox(
+            height: 80,
+            child: ListView.builder(
+              itemCount: 6,
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemBuilder: (_, index) {
+                return TVerticalImageText(
+                  image: TImages.sportIcon,
+                  title: 'Спорт',
+                  onTap: () {},
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
